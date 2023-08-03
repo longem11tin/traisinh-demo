325943907b4eb0303055135d8fe7762963ba3f88
Long Be <longnguyen.open@gmail.com>
2023-08-03 15:43:18 +0300

    demo trai sinh before have main code

---
+++ form/index.js
@@ -0,0 +1,59 @@
+$(function(){
+	$("#wizard").steps({
+        headerTag: "h4",
+        bodyTag: "section",
+        transitionEffect: "fade",
+        enableAllSteps: true,
+        transitionEffectSpeed: 400,
+        onStepChanging: function (event, currentIndex, newIndex) { 
+            if ( newIndex === 1 ) {
+                $('.steps ul').addClass('step-2');
+            } else {
+                $('.steps ul').removeClass('step-2');
+            }
+            if ( newIndex === 2 ) {
+                $('.steps ul').addClass('step-3');
+            } else {
+                $('.steps ul').removeClass('step-3');
+            }
+
+            if ( newIndex === 3 ) {
+                $('.steps ul').addClass('step-4');
+                $('.actions ul').addClass('step-last');
+            } else {
+                $('.steps ul').removeClass('step-4');
+                $('.actions ul').removeClass('step-last');
+            }
+            return true; 
+        },
+        labels: {
+            finish: "Submit",
+            next: "Next",
+            previous: "Previous"
+        },
+        names: {
+            finish: "submit",
+        },
+        types: {
+            finish: "submit",
+        }
+    });
+    // Custom Steps Jquery Steps
+    $('.wizard > .steps li a').click(function(){
+        $(this).parent().addClass('checked');
+		$(this).parent().prevAll().addClass('checked');
+		$(this).parent().nextAll().removeClass('checked');
+    });
+    // Custom Button Jquery Steps
+    $('.forward').click(function(){
+        $("#wizard").steps('next');
+    })
+    $('.backward').click(function(){
+        $("#wizard").steps('previous');
+    })
+    // Checkbox
+    $('.checkbox-circle label name type').click(function(){
+        $('.checkbox-circle label name type').removeClass('active');
+        $(this).addClass('active');
+    })
+})
